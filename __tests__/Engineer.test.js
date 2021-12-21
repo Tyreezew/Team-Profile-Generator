1a6a20526aa445b13529af88dad57d661092357d
Tyreeze <wedding5@msu.edu>
2021-12-21 14:06:40 -0600

    engineer test file and engineer.js past test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require('../lib/Engineer');
+
+test('should create an Engineer object', () => {
+    const engineer = new Engineer('Peter', 100, 'peterparker@gmail.com', 'petepark02');
+    expect(engineer.github).toEqual(expect.any(String));
+
+});
+
+test('should get engieer github username', () => {
+    const github = 'petepark02'
+    const engineer = new Engineer('Peter', 100, 'peterparker@gmail.com', github);
+    expect(engineer.getGithub()).toEqual(github)
+});
+
+test('should return engineer role', () => {
+    const role='Engineer';
+    const engineer = new Engineer('100', 'Peter', 'peterparker@gmail.com', 'petepark02');
+    expect(engineer.getRole()).toEqual(role);
+
+});
